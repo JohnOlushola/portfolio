f20a8227459387161880fc7e6c163ac4faa1d304
JohnOlushola <johntemiloluwa.o@gmail.com>
2023-04-10 20:45:37 +0100

    feat(header): link back to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface HeaderProps {
   page?: string;
 }
@@ -5,15 +7,17 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ page }) => {
   return (
     <div className="flex justify-between mb-4">
-      <div className="group border-y border-black w-fit inline-flex items-center">
-        <p className="text-xl group-hover:hidden">
-          JTO
-          {page && <span>&nbsp;&mdash;&nbsp; {page}</span>}
-        </p>
-        <p className="text-xl text-opacity-50 hidden group-hover:block">
-          John Temiloluwa Olushola
-        </p>
-      </div>
+      <Link href="/">
+        <div className="group border-y border-black w-fit inline-flex items-center">
+          <p className="text-xl group-hover:hidden">
+            JTO
+            {page && <span>&nbsp;&mdash;&nbsp; {page}</span>}
+          </p>
+          <p className="text-xl text-opacity-50 hidden group-hover:block">
+            John Temiloluwa Olushola
+          </p>
+        </div>
+      </Link>
 
       <p className="underline">work in progress!</p>
     </div>
