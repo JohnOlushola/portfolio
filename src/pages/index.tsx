b332be4f6f3311ef7dbaacc4479a6097ab597bc3
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2023-08-16 01:26:04 +0100

    feat: dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,19 @@ export default function Home() {
     <>
       <PageHead title="JTOLUSHOLA" description="John Temiloluwa Olushola" />
 
-      <div className="landing-container">
-        <div className="flex justify-between border-y border-black py-2.5">
+      <div className="landing-container dark:text-white dark:border-white">
+        <div className="flex justify-between border-y border-black dark:border-white py-2.5">
           <h2 className="text-xl md:text-4xl">engineering</h2>
           <h2 className="text-xl md:text-4xl">me</h2>
           <h2 className="text-xl md:text-4xl">photography</h2>
         </div>
 
-        <div className="h-auto mt-auto border-y border-black text-justify">
-          <svg viewBox="0 0 100 18" xmlns="http://www.w3.org/2000/svg">
+        <div className="h-auto mt-auto border-y border-black dark:border-white text-justify">
+          <svg
+            viewBox="0 0 100 18"
+            xmlns="http://www.w3.org/2000/svg"
+            className="dark:fill-white"
+          >
             <text x="2" y="14">
               JTOLUSHOLA
             </text>
