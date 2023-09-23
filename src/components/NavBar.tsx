cb0fb8dac1a83d1eee8a2ba22cdc51fba6d00cd5
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2023-09-23 20:38:26 +0100

    feat: NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div className="flex justify-between border-y py-2.5">
+      <h2 className="text-xl md:text-3xl">engineering</h2>
+      <h2 className="text-xl md:text-3xl">me</h2>
+      <h2 className="text-xl md:text-3xl">photography</h2>
+    </div>
+  );
+}
