916cf8d75a89647d8988139d9f14935b91b8cd65
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-02-18 19:23:00 +0000

    feat: vercel comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
+import { VercelToolbar } from "@vercel/toolbar/next";
 
 // Font files can be colocated inside of `pages`
 const customFont = localFont({
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={customFont.className}>
       <Component {...pageProps} />
+      <VercelToolbar />
     </main>
   );
 }
