4124684d42616a3ed92d5849733185e0e493f3e8
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-02-22 09:16:40 +0000

    feat: remove writings from projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -2,20 +2,20 @@ import { ImageLoaderProps } from "next/image";
 import { Resource } from "../types/resource";
 
 const projects: Resource[] = [
-  {
-    title: "New Year's Resolutions? No",
-    date: 2020,
-    link: {
-      url: "https://medium.com/@jtolushola/new-years-resolutions-no-d0de5e115371",
-      external: true,
-    },
-    description: "New year, new me? Nah",
-    src: "/images/logos.webp",
-    media: "image",
-    // imageLoader: ({ src }: ImageLoaderProps) => {
-    //   return src;
-    // },
-  },
+  // {
+  //   title: "New Year's Resolutions? No",
+  //   date: 2020,
+  //   link: {
+  //     url: "https://medium.com/@jtolushola/new-years-resolutions-no-d0de5e115371",
+  //     external: true,
+  //   },
+  //   description: "New year, new me? Nah",
+  //   src: "/images/logos.webp",
+  //   media: "image",
+  //   // imageLoader: ({ src }: ImageLoaderProps) => {
+  //   //   return src;
+  //   // },
+  // },
   {
     title: "Algorithmic Bias Mitigation",
     date: 2022,
@@ -72,28 +72,28 @@ const projects: Resource[] = [
       external: true,
     },
   },
-  {
-    title: "Debugging is an Art",
-    date: 2019,
-    link: {
-      url: "https://jtolushola.medium.com/debugging-is-an-art-530b0323f9e5",
-      external: true,
-    },
-    description: "Opinionated take on debugging, written by my younger self",
-    src: "/videos/laurent.mp4",
-    media: "video",
-  },
-  {
-    title: "Starting Your Journey as a Developer",
-    date: 2019,
-    link: {
-      url: "https://jtolushola.medium.com/debugging-is-an-art-530b0323f9e5",
-      external: true,
-    },
-    description: "The beginning, enjoy!",
-    src: "/images/color.webp",
-    media: "image",
-  },
+  // {
+  //   title: "Debugging is an Art",
+  //   date: 2019,
+  //   link: {
+  //     url: "https://jtolushola.medium.com/debugging-is-an-art-530b0323f9e5",
+  //     external: true,
+  //   },
+  //   description: "Opinionated take on debugging, written by my younger self",
+  //   src: "/videos/laurent.mp4",
+  //   media: "video",
+  // },
+  // {
+  //   title: "Starting Your Journey as a Developer",
+  //   date: 2019,
+  //   link: {
+  //     url: "https://jtolushola.medium.com/debugging-is-an-art-530b0323f9e5",
+  //     external: true,
+  //   },
+  //   description: "The beginning, enjoy!",
+  //   src: "/images/butterfly-1.webp",
+  //   media: "image",
+  // },
   {
     title: "Bookstore",
     date: 2020,
