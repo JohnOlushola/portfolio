f6285000fe710887ff25462bb3facfcc57566bc3
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-03-12 21:14:37 +0000

    feat: home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function Header() {
+  const { pathname } = useRouter();
+
+  const page = pathname === "/" ? "Holla" : "Craft";
+
   return (
     <header>
       <nav className="flex justify-between items-center border-b py-2.5">
         <Link href="/" className="text-opacity-60 italic">
-          &#8212; Craft
+          {page} &#8212;
         </Link>
       </nav>
     </header>
