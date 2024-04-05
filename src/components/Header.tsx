cea9277c97d356054e182852ff70ef76afe15243
JohnOlushola <johntemiloluwa.o@gmail.com>
2024-04-05 16:14:29 -0400

    chore: partial clean up classnames

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header>
       <nav className="flex justify-between items-center border-b py-2.5">
-        <h1 className="italic">jtolushola &#8212;</h1>
+        <p className="italic">jtolushola &#8212;</p>
       </nav>
     </header>
   );
