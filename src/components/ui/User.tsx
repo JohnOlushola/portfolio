8ea7a8e3ee9c767e9baf0d4afb39acd0441dccce
JohnOlushola <johntemiloluwa.o@gmail.com>
2024-04-06 15:56:36 -0400

    feat: use custom chip

--- src/components/ui/User.tsx
+++ src/components/ui/User.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Avatar from "./Avatar";
-import { badgeVariants } from "./badge";
 import { cn } from "@/lib/utils";
 
 interface UserProps {
@@ -16,8 +15,7 @@ export default function User({ url, image, fallback, username }: UserProps) {
   return (
     <Link
       className={cn(
-        badgeVariants({ variant: "outline" }),
-        "font-normal text-white text-base gap-1.5 p-1.5 py-1 no-underline"
+        "inline-flex items-center justify-center w-auto border rounded-full gap-1.5 p-1.5 py-1 no-underline"
       )}
       href={url}
     >
