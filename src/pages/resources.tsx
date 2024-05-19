40cd3cf6595f89b65c39a98152a6c66402c8de1c
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-05-19 21:58:33 +0100

    feat: SEO

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,18 +1,27 @@
+import PageHead from "@/components/PageHead";
+
 export default function ResourcesPage() {
   return (
-    <div>
-      <h2 className="mb-1">Resources</h2>
-      <p className="mb-6 md:w-2/6">
-        A collection of resources I find interesting. I am constantly adding to
-        this list. Feel free to explore.
-      </p>
+    <>
+      <PageHead
+        title="Resources"
+        description="A collection of resources I find interesting."
+      />
 
-      <div className="relative w-screen left-1/2 right-1/2 -ml-[50vw] -mr-[50vw]">
-        <iframe
-          className="border-0 w-full h-screen rounded-none"
-          src="https://raindrop.io/jtolushola/all-44362671/embed/sort=-created"
-        />
+      <div>
+        <h2 className="mb-1">Resources</h2>
+        <p className="mb-6 md:w-2/6">
+          A collection of resources I find interesting. I am constantly adding
+          to this list. Feel free to explore.
+        </p>
+
+        <section className="relative w-screen left-1/2 right-1/2 -ml-[50vw] -mr-[50vw]">
+          <iframe
+            className="border-0 w-full h-screen rounded-none"
+            src="https://raindrop.io/jtolushola/all-44362671/embed/sort=-created"
+          />
+        </section>
       </div>
-    </div>
+    </>
   );
 }
