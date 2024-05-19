2d27a37e24c50ddb87fab2ef3df097ffce5b5de9
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-05-19 22:00:14 +0100

    fix: remove unnedded div

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -8,20 +8,18 @@ export default function ResourcesPage() {
         description="A collection of resources I find interesting."
       />
 
-      <div>
-        <h2 className="mb-1">Resources</h2>
-        <p className="mb-6 md:w-2/6">
-          A collection of resources I find interesting. I am constantly adding
-          to this list. Feel free to explore.
-        </p>
+      <h2 className="mb-1">Resources</h2>
+      <p className="mb-6 md:w-2/6">
+        A collection of resources I find interesting. I am constantly adding to
+        this list. Feel free to explore.
+      </p>
 
-        <section className="relative w-screen left-1/2 right-1/2 -ml-[50vw] -mr-[50vw]">
-          <iframe
-            className="border-0 w-full h-screen rounded-none"
-            src="https://raindrop.io/jtolushola/all-44362671/embed/sort=-created"
-          />
-        </section>
-      </div>
+      <section className="relative w-screen left-1/2 right-1/2 -ml-[50vw] -mr-[50vw]">
+        <iframe
+          className="border-0 w-full h-screen rounded-none"
+          src="https://raindrop.io/jtolushola/all-44362671/embed/sort=-created"
+        />
+      </section>
     </>
   );
 }
