176aef0b8d74ff8231767cd8d865c0f4ad0de1a4
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-09-17 07:09:06 +0100

    feat: vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { VercelToolbar } from "@vercel/toolbar/next";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 // Font files can be colocated inside of `pages`
 const customFont = localFont({
@@ -28,6 +29,7 @@ export default function Nextra({ Component, pageProps }: AppProps) {
         />
       </Head>
       <div className={`${customFont.className} flex flex-col min-h-screen`}>
+        <Analytics />
         <Header />
         <main className="my-20 container">
           <Component {...pageProps} />
