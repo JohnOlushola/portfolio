d261db608f640c07b1be11ccfbffabc2aab2100e
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2025-04-18 22:40:32 +0100

    feat: side nav

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,11 @@ import PreviewedLink from "@/components/PreviewedLink";
 import WorkItem from "@/components/WorkItem";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 export interface ContentItem {
+  id: string;
   name: string;
   link: string;
   asset?: string;
@@ -15,12 +17,14 @@ export interface ContentItem {
 
 const projects: ContentItem[] = [
   {
+    id: "scenario-builder",
     name: "Scenario Builder",
     link: "https://solar.transitionzero.org/",
     asset: "/videos/scenario-builder.mp4",
     year: 2025,
   },
   {
+    id: "sam",
     name: "Solar Asset Mapper",
     link: "https://solar.transitionzero.org/",
     asset:
@@ -28,12 +32,14 @@ const projects: ContentItem[] = [
     year: 2024,
   },
   {
+    id: "tjwho",
     name: "TJWHO Universe",
     link: "https://www.tjwho.co/",
     asset: "/videos/tjwho-main.mp4",
     year: 2023,
   },
   {
+    id: "airpods-pro",
     name: "Airpods Pro",
     link: "https://airpods-pro.jtolushola.com/",
     asset: "/videos/airpods.mov",
@@ -43,22 +49,27 @@ const projects: ContentItem[] = [
 
 const notes: ContentItem[] = [
   {
+    id: "typing-a-slug",
     name: "Typing a slug",
     link: "/notes/typing-a-slug",
   },
   {
+    id: "state-machines-a-simple-introduction",
     name: "State machines - a simple introduction",
     link: "/notes/state-machines",
   },
   {
+    id: "on-keeping-things-simple",
     name: "On Keeping things simple",
     link: "/notes/state-machines",
   },
   {
+    id: "a-space-to-share-resources",
     name: "A space to share resources",
     link: "/notes/state-machines",
   },
   {
+    id: "more",
     name: "...more",
     link: "/notes",
   },
@@ -67,10 +78,12 @@ const notes: ContentItem[] = [
 export default function Home() {
   const [activeTab, setActiveTab] = useState("works");
 
+  const { pathname } = useRouter();
+
   return (
     <>
       <PageHead />
-      <div className="font-mono text-center mt-20 justify-center flex flex-col items-center relative">
+      <div className="font-mono text-center mt-20 justify-center flex flex-col items-center relative scroll-smooth">
         <h1 className="mb-8">
           Temiloluwa Olushola &mdash;{" "}
           <span className="opacity-60">design, engineering, AI;</span>{" "}
@@ -92,8 +105,8 @@ export default function Home() {
             ))}
           </ol>
         ) : activeTab === "works" ? (
-          projects.map(({ name, asset, link }, index) => (
-            <WorkItem key={index} name={name} asset={asset} link={link} />
+          projects.map((project, index) => (
+            <WorkItem key={index} {...project} />
           ))
         ) : (
           <iframe
@@ -101,6 +114,26 @@ export default function Home() {
             src="https://raindrop.io/jtolushola/all-44362671/embed/sort=-created"
           />
         )}
+
+        <div className="fixed inset-y-0 right-0 flex flex-col justify-center h-screen p-4 space-y-4 group">
+          {projects.map(({ id }, index) => (
+            <div key={id}>
+              <Link
+                href={`/#${id}`}
+                className={cn(
+                  {
+                    "decoration-accent text-accent": pathname.includes(
+                      `/#${id}`
+                    ),
+                  },
+                  "opacity-40 group-hover:opacity-100 transition-opacity ease-in-out"
+                )}
+              >
+                {index}
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
