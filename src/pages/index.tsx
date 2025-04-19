1d11d4695a67b8b0210e70b989afe0f393e36ccb
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2025-04-19 16:30:00 +0100

    feat: include links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -94,26 +94,41 @@ export default function Home() {
 
           <div className="mt-6 leading-relaxed space-y-6">
             <p>
-              I’m currently a Senior Frontend Engineer at TransitionZero, where
-              I’m helping build a platform for energy systems modelling. I also
-              contributed to the development of Solar Asset Mapper, a
-              planetary-scale dataset of medium to large-scale solar power
+              I’m currently a Senior Frontend Engineer at{" "}
+              <a
+                href="https://www.transitionzero.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                TransitionZero
+              </a>
+              , where I’m helping build a platform for energy systems modelling.
+              I also contributed to the development of{" "}
+              <PreviewedLink
+                href="https://solar.transitionzero.org/"
+                asset="https://res.cloudinary.com/jtolushola/video/upload/f_auto:video,q_auto/v1/portfolio/high_obnhlw"
+              >
+                Solar Asset Mapper
+              </PreviewedLink>{" "}
+              , a planetary-scale dataset of medium to large-scale solar power
               plants.
             </p>
             <p>
-              Outside of work, I’m a gym rat, music lover, car enthusiast,
-              outdoorsy type—and most importantly, a child of God. Lately, I’ve
-              been learning to dance Bachata and it’s been a blast. This site is
-              my little home on the internet.
+              Outside of my work-related passions, I’m a gym rat, music lover,
+              car enthusiast, outdoorsy type—and most importantly, a child of
+              God. Lately, I’ve been learning to dance Bachata and it’s been a
+              blast. This site is my little home on the internet.
             </p>
             <p>
-              In /notes, you’ll find half-baked thoughts, learning snippets,
-              technical writing, and anything else I feel like dropping here.
-              Check out /resources for a curated list of cool stuff—people,
-              blogs, tools, ideas—that I’ve bookmarked. It’s mainly for me, but
-              hopefully helpful for you too. In /works, you’ll find a few
-              selected projects I’ve worked on. Thanks for stopping by—feel free
-              to reach out. Cheers!
+              In <Link href="/notes">/notes</Link>, you’ll find half-baked
+              thoughts, learning snippets, technical writing, and anything else
+              I feel like dropping here. Check out{" "}
+              <Link href="/resources">/resources</Link> for a curated list of
+              cool stuff—people, blogs, tools, ideas—that I’ve bookmarked. It’s
+              mainly for me, but hopefully helpful for you too. In{" "}
+              <Link href="/works">/works</Link> , you’ll find a few selected
+              projects I’ve worked on. Thanks for stopping by, feel free to
+              reach out. Cheers!
             </p>
           </div>
         </details>
