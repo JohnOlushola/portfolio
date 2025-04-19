822c6f86780899258409e39c5b11a85c81f00fbe
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2025-04-19 21:55:51 +0100

    fix: focus on article item

--- src/components/WorkItem.tsx
+++ src/components/WorkItem.tsx
@@ -1,21 +1,21 @@
 import { ContentItem } from "@/pages";
 import React from "react";
 
-export default function WorkItem({ link, asset, name, id }: ContentItem) {
+export default function WorkItem({ link, asset, name, id, year }: ContentItem) {
   return (
     <article id={id}>
       <a
         href={link}
         target="_blank"
         rel="noopener noreferrer"
-        className="block"
+        className="block group focus:outline-none"
       >
         <video
           muted
           loop
           autoPlay
           controls={false}
-          className="w-full lg:w-[70vw] h-full mb-1 rounded-xl overflow-hidden peer border-2 hover:border-accent hover:p-1 transition-all"
+          className="w-full lg:w-[70vw] h-full mb-1 rounded-xl overflow-hidden border-2 group-focus:border-accent hover:border-accent group-focus:p-1 hover:p-1 transition-all"
           src={asset}
         />
         <div className="flex justify-between">
