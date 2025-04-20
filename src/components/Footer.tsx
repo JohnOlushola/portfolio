e6ed22415eb3454d37441186f1147db8f2714b01
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2025-04-20 20:30:42 +0100

    fix: responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="mt-auto">
       <div className="container flex flex-col space-y-4 items-center py-8 text-sm">
         <div className="flex items-center gap-8">
+          <Link href="/resources">resources</Link>
           <a
             href="https://github.com/JohnOlushola"
             target="_blank"
             rel="noopener noreferrer"
-            className="mr-4"
           >
             code
           </a>
@@ -15,7 +17,6 @@ export default function Footer() {
             href="mailto:johntemiloluwa.o@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
-            className="mr-4"
           >
             say hi
           </a>
