79dd490a7738ec1521f511c34d709f822c1105ed
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2025-04-20 20:30:42 +0100

    fix: responsiveness

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -10,7 +10,7 @@ export default function Notes() {
   );
 
   return (
-    <ol className="space-y-4 mx-auto w-full lg:w-[80vw] list-decimal">
+    <ol className="space-y-8 md:space-y-4 mx-auto w-full lg:w-[80vw] ml-8 md:ml-0 list-decimal">
       {notes.map(({ slug, metadata }) => {
         const LinkComp = metadata.image ? PreviewedLink : Link;
 
@@ -21,7 +21,7 @@ export default function Notes() {
               href={`/notes/${slug}`}
               className="flex flex-col space-y-1"
             >
-              <div className="w-full flex flex-col justify-between md:flex-row md:items-baseline md:space-x-10">
+              <div className="w-full flex flex-col space-y-2 justify-between md:flex-row md:items-baseline md:space-x-10">
                 <p className="tracking-tight !text-foreground">{metadata.title}</p>
                 <p className="tabular-nums text-muted-foreground">
                   {formatDate(metadata.publishedAt, false)}
